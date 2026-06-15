d0bbb642b2eb3dd7350e7ba53e9f4b2fd1e2dc3c
tarunjtalurmath-ctrl <tarun.j.talurmath@gmail.com>
2026-06-15 23:10:25 +0530

    Add files via upload

---
+++ Data/intial_parameters.m
@@ -0,0 +1,14 @@
+% EV physical parameters 
+m=1300; %this mass of sedan (kg)
+Cd=0.23; %drag coefficient of the vehicle 
+A= 2.2; % Surface Area of the vehicle (m^2)
+g=9.81; %gravity (m/sec^2)
+rho=1.2; %Air density (kg/m^3)
+R_tire=0.3; %radius of tire (m)
+Cr=0.015; %rolling resistance coefficient 
+V_nominal=350; % Same battery voltage as in sedan (V)
+R_internal=0.1; % internal resistance (ohms)
+J_motor=0.05; % Motor rotor inertia (kg*m^2)
+% hello world
+
+
